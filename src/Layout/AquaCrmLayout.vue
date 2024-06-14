2ad6a34b7ade42de8235eeafe1b4084f5c9c08e2
SVSKHD <8svskhd@gmail.com>
2024-06-15 00:09:28 +0530

    app and components

---
+++ src/Layout/AquaCrmLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-app-bar collapse :elevation="24">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-heart"></v-btn>
+
+          <v-btn icon="mdi-magnify"></v-btn>
+
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="toggleDrawer" temporary>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" title="John Leider"></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <div class="d-flex justify-center align-center h-100">
+          <router-view />
+        </div>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import { ref } from "vue"
+export default {
+
+  setup() {
+    let toggleDrawer = ref(false)
+    return {
+      toggleDrawer,
+    }
+  },
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import AquaCrmLayout from "../Layout/AquaCrmLayout.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/layout',
+      name: 'Layout',
+      component: AquaCrmLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/AboutView.vue')
+        }
+      ]
     }
   ]
 })
