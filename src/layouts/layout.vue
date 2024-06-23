f7b9a19af2c746ff2f8217c43bb7b44cd86c139a
SVSKHD <8svskhd@gmail.com>
2024-06-23 18:47:57 +0530

    main and index and lock files

---
+++ src/layouts/layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card
+    color="grey-lighten-4"
+    rounded="0"
+    flat
+  >
+    <v-toolbar collapse>
+      <v-btn icon>
+        <v-icon icon="fas fa-home" />
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,29 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { aliases, fa } from 'vuetify/iconsets/fa-svg'
 
+
+const AQUA = createApp(App)
+AQUA.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas) // Include needed solid icons
+library.add(far) // Include needed regular icons
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
 })
 
-createApp(App).use(store).use(router).use(vuetify).mount('#app')
+
+AQUA.use(store).use(router).use(vuetify).mount('#app')
