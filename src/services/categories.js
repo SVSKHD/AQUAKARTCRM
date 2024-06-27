9a36a82b2533bca87d0d03cbbbff9cfd4decba2a
SVSKHD <8svskhd@gmail.com>
2024-06-28 00:28:32 +0530

    categories and sub categories

--- src/services/categories.js
+++ src/services/categories.js
@@ -6,9 +6,32 @@ const getAllcategories = async () => await axios.get(`${BASE}/allcategories`);
 
 const getCategory = (id) => axios.get(`${BASE}/category/${id}`);
 
+const EditCategory = (id, token , data) => axios.post(`${BASE}/category-add/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+},data);
+
+const CreateCategory = (id, token , data) => axios.put(`${BASE}/category-update/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+}, data);
+
+const DeleteCategory = (id, token) => axios.get(`${BASE}/category/delete/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+});
+
+
+
 const CategoryServiceOperarations = {
   getAllcategories,
   getCategory,
+  EditCategory,
+  DeleteCategory,
+  CreateCategory
 };
 
 export default CategoryServiceOperarations;
--- src/services/subCategories.js
+++ src/services/subCategories.js
@@ -6,9 +6,32 @@ const getAllSubCategories = async () => await axios.get(`${BASE}/all-subcategori
 
 const getSubCategory = (id) => axios.get(`${BASE}/subcategory/${id}`);
 
+
+const EditSubCategory = (id, token , data) => axios.post(`${BASE}/subcategory-add/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+},data);
+
+const CreateSubCategory = (id, token , data) => axios.put(`${BASE}/subcategory-update/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+}, data);
+
+const DeleteSubCategory = (id, token) => axios.get(`${BASE}/subcategory/delete/${id}`,{
+  headers:{
+    Authorization:`Bearer ${token}`
+  }
+});
+
+
 const SubCategoryServiceOperarations = {
   getAllSubCategories,
   getSubCategory,
+  EditSubCategory,
+  CreateSubCategory,
+  DeleteSubCategory
 };
 
 export default SubCategoryServiceOperarations;
